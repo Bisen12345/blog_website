c2f8acff92032d861065f4ac525372b49fef0094
Bisen12345 <88203130+Bisen12345@users.noreply.github.com>
2023-07-15 15:16:53 +0530

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from app.models import Post
+
+def BASE(request):
+    return render(request,'main/base.html')
+
+def INDEX(request):
+    popular_post=Post.objects.filter(section='Popular',status=1).order_by('-id')[0:4]
+    recent_post=Post.objects.filter(section='Recent',status=1).order_by('-id')[0:4]
+    main_post=Post.objects.filter(Main_post=True,status=1)[0:1]
+    Editor_Pick=Post.objects.filter(section='Editors_Pick',status=1).order_by('-id')
+    Trending=Post.objects.filter(section='Trending',status=1).order_by('-id')
+    Inspiraton=Post.objects.filter(section='Inspiration',status=1).order_by('-id')[0:2]
+    Latest_Posts=Post.objects.filter(section='Latest Posts',status=1).order_by('-id')[0:4]
+
+
+    context = {
+      'popular_post':popular_post,
+      'recent_post':recent_post,
+      'main_post': main_post,
+      'Editor_Pick': Editor_Pick,
+      'Trending': Trending,
+      'Inspiraton':Inspiraton,
+      'Latest_Posts':Latest_Posts
+
+   }
+    return render(request,'main/index.html',context)
+    
